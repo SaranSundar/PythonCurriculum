10743ca0d17fc575f0e706db8d181cb65c16edf7
SaranSundar <saransszg@gmail.com>
2021-01-07 20:42:59 -0600

    Projects complete

---
+++ DayByDayLessons/Day1/day1_projects.py
@@ -0,0 +1,38 @@
+# Part 3 - Project: Letter to the Future
+career = "Software Engineer"
+goal1 = "climb mount everest"
+goal2 = "become a great chef"
+goal3 = "learn scuba diving"
+accomplishment1 = "won first place in a marathon"
+accomplishment2 = "traveled to 30 different countries"
+print("Dear future me,")
+print("")
+print("I hope that when you look back on this letter you will be having a successful life as a " + career + ".")
+print("As of the me right now I want to " + goal1 + ", " + goal2 + ", and " + goal3 + ".")
+print("I hope the you of the future have reached those goals.")
+print("My dream accomplishments would be to have " + accomplishment1 + " and " + accomplishment2 + ".")
+print("If you did all that, then you made my dreams come true")
+
+# Part 4 - Project: Shopping Cart
+meats = "Turkey Slices, Pepperoni, Chicken Wings"
+vegetables = "Carrots, Broccoli, Lettuce, Cabbage"
+snacks = "Chocolate Chip cookies, Fruit Snacks, Ice Cream"
+items = "Colored Pencils, Notebooks, Rulers"
+print("Don't forget to buy everything from the grocery store!")
+print("We need 3 meats, " + meats + ".")
+print("We need 4 vegetables, " + vegetables + ".")
+print("Also get 3 snacks, " + snacks + ".")
+print("And finally don't forget about " + items)
+
+# Part 5 - Project: Mad Libs
+noun1 = "Dog"
+noun2 = "Donut"
+noun3 = "Football"
+adjective1 = "Crazy"
+adjective2 = "Sparkly"
+adjective3 = "Funny"
+
+print("The " + adjective1 + " " + noun1 + " thought the " +
+      adjective2 + " " + noun3 + " was a " + noun2 +
+      " and accidentally tried to eat it! We laughed out loud because it was so " +
+      adjective3 + "!")
